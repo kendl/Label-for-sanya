5c895410738715e0615539b63c608848309fa49a
kendl <40860169+kendl@users.noreply.github.com>
2023-07-11 13:38:25 +0300

    Delete WebSiteLabel directory

--- WebSiteLabel/script.js
+++
@@ -1,5 +0,0 @@
-// Добавление обработчика события на кнопку "Listen Now"
-const listenNowButton = document.querySelector('#hero button');
-listenNowButton.addEventListener('click', () => {
-   alert('Coming soon!');
-});
