e7fc4506db5ad08f32c573a1e281c3305a597d3b
kendl <40860169+kendl@users.noreply.github.com>
2023-07-11 15:27:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Добавление обработчика события на кнопку "Listen Now"
+const listenNowButton = document.querySelector('#hero button');
+listenNowButton.addEventListener('click', () => {
+   alert('Coming soon!');
+});
